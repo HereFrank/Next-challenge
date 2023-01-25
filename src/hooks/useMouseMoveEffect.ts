6a31db997a730e7d1f8d9cbbcf2528137de890c1
Frank Agraz <frankagrazalberto@gmail.com>
2023-01-25 01:10:47 -0400

    fixed client and server error

--- src/hooks/useMouseMoveEffect.ts
+++ src/hooks/useMouseMoveEffect.ts
@@ -3,13 +3,13 @@ import { RefObject, useEffect, useState } from "react";
 import { useMouse } from "react-use";
 
 
-export const useMouseMoveEffect = (ref: RefObject<Element>) => {
+export const useMouseMoveEffect = (ref: RefObject<Element> ) => {
     const [canChange, setCanChange] = useState(true);
     const [fillColors, setFillColors] = useState({
-      rec1: getRandomRGB(),
-      rec2: getRandomRGB(),
-      rec3: getRandomRGB(),
-      rec4: getRandomRGB(),
+      rec1: "#FD01FD",
+      rec2: "#0000FE",
+      rec3: "#FE0000",
+      rec4: "#02FF02",
     });
     const { docX } = useMouse(ref)
 
